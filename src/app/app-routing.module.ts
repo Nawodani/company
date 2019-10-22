53e48f0e96d721eee3bbaf2491336713cc21c6f0
Kaawya20 <kaawyakavindee231@gmail.com>
2019-10-22 17:29:32 +0530

    Create Employee

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { EmployeeAddComponent } from './components/employee/employee-add/employee-add.component';
+import { EmployeeEditComponent } from './components/employee/employee-edit/employee-edit.component';
+import { EmployeeGetComponent } from './components/employee/employee-get/employee-get.component';
+
+const routes: Routes = [
+  {
+    path: 'employee/add',
+    component: EmployeeAddComponent
+  },
+  {
+    path: 'edit/:id',
+    component: EmployeeEditComponent
+  },
+  {
+    path: 'employees',
+    component: EmployeeGetComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,41 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeeAddComponent } from './components/employee/employee-add/employee-add.component';
+import { EmployeeGetComponent } from './components/employee/employee-get/employee-get.component';
+import { EmployeeEditComponent } from './components/employee/employee-edit/employee-edit.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSelectModule } from '@angular/material';
+import { SlimLoadingBarModule } from 'ng2-slim-loading-bar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { ReactiveFormsModule, FormsModule  } from '@angular/forms';
+import { MatInputModule } from '@angular/material';
+import { HttpClientModule } from '@angular/common/http';
+
+import { EmployeeService } from './core/services/employee.service';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeAddComponent,
+    EmployeeGetComponent,
+    EmployeeEditComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSelectModule,
+    SlimLoadingBarModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    FormsModule,
+    HttpClientModule
+    
   ],
-  providers: [],
+  providers: [ EmployeeService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
